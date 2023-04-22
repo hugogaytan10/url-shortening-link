fd1bb4be0fa1f5ac6c9fe2b5a54a8c81ff5f69b7
hugogaytan10 <estardus9@gmail.com>
2023-04-22 11:16:10 -0600

    no se pudo

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cors from 'vite-plugin-cors';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    cors({
-      origin: '*', // o la URL que desees permitir
-      // puedes agregar otros encabezados aquí si es necesario
-    })],
-  
+  plugins: [react()]
 })
